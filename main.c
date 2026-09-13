8a79518d95767767afad38782e8db9739863855e
d3fd44 <mhdmomendefda359@gmail.com>
2026-09-14 02:01:52 +0300

    add better error names and messages with proper handling

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 
 // propably overkill. just wanna be cool lol.
 #define ERRORS(T)                                                                                                                          \
-    T(SD_ERR, "socket error: couldn't initialize file descriptor.")                                                                        \
+    T(SOCK_DESC_ERR, "socket error: couldn't initialize file descriptor.")                                                                 \
+    T(CONNECTION_FAIL, "couldn't connect to the destination")                                                                              \
     T(FS_ERR, "fs error: error reading file.")                                                                                             \
-    T(AL_ERR, "memory allocation failed.")                                                                                                 \
-    T(BD_ARG, "bad arguments.")                                                                                                            \
-    T(AD_ERR, "address error")                                                                                                             \
-    T(CONNECTION_FAILED, "couldn't connect to the destination")
+    T(ALLOC_ERR, "memory allocation failed.")                                                                                              \
+    T(BAD_ARGS_ERR, "bad arguments.")                                                                                                      \
+    T(DST_ADD_ERR, "specified address contains characters representing a non-valid address in the specified address family")               \
+    T(UNHANDLED_ERR, "wtf")
 
 typedef enum
 {
@@ -20,16 +21,16 @@ typedef enum
 #undef T
       NO_ERR = -1
 
-} throw_err_t;
+} terr_t;
 
-static throw_err_t terr = -1;
-const char        *err_msg[] = {
+static terr_t terr = -1;
+const char   *err_msg[] = {
 #define T(err, errmsg) [err] = errmsg,
     ERRORS(T)
 #undef T
 };
 
-#define THROW_FAIL(code)                                                                                                                   \
+#define TFAIL(code)                                                                                                                        \
     do                                                                                                                                     \
     {                                                                                                                                      \
         terr = (code);                                                                                                                     \
@@ -40,15 +41,16 @@ int main(int argc, char **argv)
 {
     if (argc < 3)
     {
-        printf("usage: throw <dest-ip> <file>\n");
-        THROW_FAIL(BD_ARG);
+        printf("usage: toss <dest-ip> <file>\n");
+        TFAIL(BAD_ARGS_ERR);
     }
+
 
     FILE *file = fopen(argv[2], "r");
     if (!file)
-        THROW_FAIL(FS_ERR);
+        TFAIL(FS_ERR);
 
-    fseek(file, 0, SEEK_END);
+    fseek(file, 0, SEEK_END);  // maybe stat does the thing?
     size_t fsize = ftell(file);
     fseek(file, 0, SEEK_SET);
 
@@ -56,17 +58,26 @@ int main(int argc, char **argv)
 
     int sd = socket(AF_INET, SOCK_STREAM, 0);
     if (sd == -1)
-        THROW_FAIL(SD_ERR);
+        TFAIL(SOCK_DESC_ERR);
 
     struct sockaddr_in dst = { .sin_family = AF_INET, .sin_port = htons(12345) };
 
-    if (inet_pton(AF_INET, "127.0.0.1", &dst.sin_addr) == -1)  // TODO: 0 means string missing somethign (man 3 inet_pton)
-        THROW_FAIL(AD_ERR);
+    switch (inet_pton(AF_INET, "127.0.0.1", &dst.sin_addr))
+    {
+        case -1:  // returned when `af` (arg 1) isn't a valid address family (skip for now)
+            break;
+        case 0:
+            TFAIL(DST_ADD_ERR);
+        case 1:
+            break;
+        default:
+            TFAIL(UNHANDLED_ERR);
+    }
 
     int connected = connect(sd, (struct sockaddr *)&dst, sizeof(dst));
 
     if (connected)  // non-zero on error
-        THROW_FAIL(CONNECTION_FAILED);
+        TFAIL(CONNECTION_FAIL);
 
     return 0;
 
