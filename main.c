48dcbaa358f8e4b8779c67f2a68e241a7e689922
d3fd44 <mhdmomendefda359@gmail.com>
2026-09-14 21:55:25 +0300

    add initial cli-arg parsing implementation

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <arpa/inet.h>
 #include <assert.h>
+#include <errno.h>
+#include <libgen.h>
+#include <netinet/in.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,26 +11,27 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define INVALID_AF -1
-#define BAD_ADDR   0
+#define OK 1
 
 // propably overkill. just wanna be cool lol.
 #define ERRORS(T)                                                                                                                          \
-    T(SOCK_DESC_ERR, "socket error: couldn't initialize file descriptor.")                                                                 \
-    T(CONNECTION_FAIL, "destination unreachable.")                                                                                         \
-    T(FS_ERR, "fs error: error reading file.")                                                                                             \
-    T(STAT_READ_ERR, "fs error: couldn't get file stats.")                                                                                 \
     T(ALLOC_ERR, "memory allocation failed.")                                                                                              \
     T(BAD_ARGS_ERR, "bad arguments.")                                                                                                      \
-    T(DST_ADD_ERR, "specified address contains characters representing a non-valid address in the specified address family")               \
-    T(UNHANDLED_ERR, "wtf")
+    T(CONNECTION_FAIL, "destination unreachable.")                                                                                         \
+    T(FS_ERR, "fs error: error reading file.")                                                                                             \
+    T(INVALID_ADDR, "specified address contains characters representing a non-valid address in the specified address family")              \
+    T(INVALID_HOST, "invalid port number.")                                                                                                \
+    T(SOCK_DESC_ERR, "socket error: couldn't initialize file descriptor.")                                                                 \
+    T(STAT_READ_ERR, "fs error: couldn't get file stats.")                                                                                 \
+    T(UNHANDLED_ERR, "wtf")                                                                                                                \
+    T(UNSUPPORTED_AF, "unsupported address family (IPv4 only).")
 
 typedef enum
 {
+    NO_ERR,
 #define T(err, errmsg) err,
     ERRORS(T)
 #undef T
-      NO_ERR = -1
 } terr_t;
 
 typedef struct
@@ -34,6 +39,16 @@ typedef struct
     uint32_t tf_name_len;
     uint64_t tf_size;
 } tf_header_t;
+
+typedef struct
+{
+    bool               print_usage;
+    bool               mode;
+    char              *file_path, *file_name;
+    struct stat        fstat;
+    struct sockaddr_in dst_addr;
+    terr_t             err;
+} targs_t;
 
 static terr_t terr = -1;
 const char   *err_msg[] = {
@@ -48,6 +63,78 @@ const char   *err_msg[] = {
         terr = (code);                                                                                                                     \
         goto exit;                                                                                                                         \
     } while (0)
+
+void process_args(int argc, char **argv, targs_t *targs)
+{
+    bool  print_usage = false;
+    bool  mode;
+    char *inet = { 0 };
+    int   host = 12345;
+
+    char *path = { 0 };
+    char *file_name = { 0 };
+
+    if (argc == 1)
+        print_usage = true;
+
+    targs->dst_addr.sin_family = AF_INET;  // IPv4 only for now
+
+    for (int i = 0; i < argc; i++)
+    {
+        if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0))
+        {
+            print_usage = true;
+        }
+        else if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--send") == 0))
+        {
+            if (((i + 1) < argc) && (argv[i + 1][0] != '-'))
+            {
+                switch (inet_pton(AF_INET, argv[i + 1], &targs->dst_addr.sin_addr))
+                {
+                    case -1:
+                        targs->err = UNSUPPORTED_AF;
+                        return;
+                    case 0:
+                        targs->err = INVALID_ADDR;
+                        return;
+                    case OK:
+                        break;
+                    default:
+                        targs->err = UNHANDLED_ERR;
+                        return;
+                }
+            }
+        }
+        else if ((strcmp(argv[i], "-p") == 0) || (strcmp(argv[i], "--port") == 0))
+        {
+            if (((i + 1) < argc) && (argv[i + 1][0] != '-'))
+            {
+                char *end = NULL;
+                long  value = strtol(argv[i + 1], &end, 10);
+
+                if (errno == ERANGE || end == argv[i + 1] || *end != '\0')
+                {
+                    targs->err = INVALID_HOST;
+                    return;
+                }
+
+                host = (unsigned short)value;
+                targs->dst_addr.sin_port = htons(host);  // maybe move to the end to make sure it falls back to the default value
+                i++;
+            }
+        }
+        else if (argv[i][0] != '-')
+        {
+            if (stat(argv[i], &targs->fstat))
+            {
+                targs->err = STAT_READ_ERR;
+                return;
+            }
+            targs->file_path = argv[i];
+            targs->file_name = basename(targs->file_path);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
