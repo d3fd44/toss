33856c1853703ac991629b745c523e53ba003b0f
d3fd44 <mhdmomendefda359@gmail.com>
2026-09-14 23:34:53 +0300

    refine arg parser and switch to it

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <assert.h>
 #include <errno.h>
 #include <libgen.h>
-#include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +10,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define OK 1
+#define TSEND 0
+#define TRECV 1
+#define OK    1
 
 // propably overkill. just wanna be cool lol.
 #define ERRORS(T)                                                                                                                          \
@@ -45,7 +46,7 @@ typedef struct
     bool               print_usage;
     bool               mode;
     char              *file_path, *file_name;
-    struct stat        fstat;
+    struct stat        file_stat;
     struct sockaddr_in dst_addr;
     terr_t             err;
 } targs_t;
@@ -64,6 +65,9 @@ const char   *err_msg[] = {
         goto exit;                                                                                                                         \
     } while (0)
 
+// my first argument parser, dam it feels kinda tricky. inspired by raysan simple parser:
+// https://github.com/raysan5/rfxgen/blob/3185a36277226243695169da1e0d8d4aedde6f50/src/rfxgen.c#L1125
+// error handling works, but sucks. i mean it is good, but the reporting sucks
 void process_args(int argc, char **argv, targs_t *targs)
 {
     bool  print_usage = false;
@@ -77,16 +81,19 @@ void process_args(int argc, char **argv, targs_t *targs)
     if (argc == 1)
         print_usage = true;
 
+    memset(targs, 0, sizeof *targs);
     targs->dst_addr.sin_family = AF_INET;  // IPv4 only for now
 
-    for (int i = 0; i < argc; i++)
+    for (int i = 1; i < argc; i++)
     {
         if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0))
         {
             print_usage = true;
+            break;
         }
         else if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--send") == 0))
         {
+            mode = TSEND;
             if (((i + 1) < argc) && (argv[i + 1][0] != '-'))
             {
                 switch (inet_pton(AF_INET, argv[i + 1], &targs->dst_addr.sin_addr))
@@ -103,6 +110,41 @@ void process_args(int argc, char **argv, targs_t *targs)
                         targs->err = UNHANDLED_ERR;
                         return;
                 }
+                i++;
+            }
+            else
+            {
+                printf("error parsing address after \"-s | --send\"\n");
+                targs->err = INVALID_ADDR;
+                return;
+            }
+        }
+        else if ((strcmp(argv[i], "-r") == 0) || (strcmp(argv[i], "--receive") == 0))
+        {
+            mode = TRECV;
+            if (((i + 1) < argc) && (argv[i + 1][0] != '-'))
+            {
+                switch (inet_pton(AF_INET, argv[i + 1], &targs->dst_addr.sin_addr))
+                {
+                    case -1:
+                        targs->err = UNSUPPORTED_AF;
+                        return;
+                    case 0:
+                        targs->err = INVALID_ADDR;
+                        return;
+                    case OK:
+                        break;
+                    default:
+                        targs->err = UNHANDLED_ERR;
+                        return;
+                }
+                i++;
+            }
+            else
+            {
+                printf("error parsing address after \"-r | --receive\"\n");
+                targs->err = INVALID_ADDR;
+                return;
             }
         }
         else if ((strcmp(argv[i], "-p") == 0) || (strcmp(argv[i], "--port") == 0))
@@ -119,73 +161,98 @@ void process_args(int argc, char **argv, targs_t *targs)
                 }
 
                 host = (unsigned short)value;
-                targs->dst_addr.sin_port = htons(host);  // maybe move to the end to make sure it falls back to the default value
                 i++;
+            }
+            else
+            {
+                printf("error parsing port number after \"-p | --port\"\n");
+                targs->err = INVALID_HOST;
+                return;
             }
         }
         else if (argv[i][0] != '-')
         {
-            if (stat(argv[i], &targs->fstat))
+            if (stat(argv[i], &targs->file_stat))
             {
+                printf("%s\n", argv[i]);
                 targs->err = STAT_READ_ERR;
                 return;
             }
             targs->file_path = argv[i];
             targs->file_name = basename(targs->file_path);
         }
+        else
+        {
+            printf("unknown argument: %s\n", argv[i]);
+            targs->err = BAD_ARGS_ERR;
+            return;
+        }
     }
+
+    targs->err = NO_ERR;
+    targs->print_usage = print_usage;
+    targs->dst_addr.sin_port = htons(host);
+    targs->mode = mode;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
+    targs_t targs;
+    process_args(argc, argv, &targs);
+
+    if (targs.err != NO_ERR)
     {
-        printf("usage: toss <dest-ip> <file>\n");
-        TFAIL(BAD_ARGS_ERR);
+        printf("usage: toss [<options>] <path-to-file>\n"
+               "options:\n"
+               "    -s, --send    <destination-ip>\n"
+               "    -r, --receive <bind-ip>\n"
+               "    -p, --port    <port-number>\n"
+               "    -h, --help\n\n");
+        TFAIL(targs.err);
     }
 
-    struct stat file_stat;
+    if (targs.print_usage == true)
+    {
+        // repeated
+        printf("usage: toss [<options>] <path-to-file>\n"
+               "options:\n"
+               "    -s, --send    <destination-ip>\n"
+               "    -r, --receive <bind-ip>\n"
+               "    -p, --port    <port-number>\n"
+               "    -h, --help\n\n");
+        return 0;
+    }
+
+    char in_buf[INET_ADDRSTRLEN];
+
+    // construct a message header
     tf_header_t file_header;
 
-    if (stat(argv[2], &file_stat))
-        TFAIL(STAT_READ_ERR);
-
-    FILE *file = fopen(argv[2], "r");
+    FILE *file = fopen(targs.file_path, "r");
     if (!file)
         TFAIL(FS_ERR);
 
-    file_header.tf_name_len = strlen(argv[2]);
-    file_header.tf_size = file_stat.st_size;
+    file_header.tf_name_len = strlen(targs.file_name);
+    file_header.tf_size = targs.file_stat.st_size;
 
     int sd = socket(AF_INET, SOCK_STREAM, 0);
     if (sd == -1)
         TFAIL(SOCK_DESC_ERR);
 
-    struct sockaddr_in dst_addr = { .sin_family = AF_INET, .sin_port = htons(12345) };
-
-    switch (inet_pton(AF_INET, "127.0.0.1", &dst_addr.sin_addr))
+    if (connect(sd, (struct sockaddr *)&targs.dst_addr, sizeof(targs.dst_addr)))
     {
-        case INVALID_AF:  // returned when `af` (arg 1) isn't a valid address family (skip for now)
-            break;
-        case BAD_ADDR:
-            TFAIL(DST_ADD_ERR);
-        case 1:
-            break;
-        default:
-            TFAIL(UNHANDLED_ERR);
+        perror("smth\n");
+        TFAIL(CONNECTION_FAIL);
     }
 
-    if (connect(sd, (struct sockaddr *)&dst_addr, sizeof(dst_addr)))
-        TFAIL(CONNECTION_FAIL);
-
-    printf("sending \"%s\" (%zu Bytes)...\n", argv[2], file_header.tf_size);
+    printf("sending \"%s\" (%zu Bytes)...\n", targs.file_path, file_header.tf_size);
 
     assert(sizeof(tf_header_t) == send(sd, &file_header, sizeof(tf_header_t), 0));
-    assert(file_header.tf_name_len == send(sd, argv[2], file_header.tf_name_len, 0));
+    assert(file_header.tf_name_len == send(sd, targs.file_name, file_header.tf_name_len, 0));
 
     char buf[1024] = { 0 };
 
-    for (size_t i = 0; i < (file_stat.st_size / _Countof(buf)) + 1; i++)
+    for (size_t i = 0; i < (targs.file_stat.st_size / _Countof(buf)) + 1; i++)
     {
         int read_count = fread(buf, 1, _Countof(buf), file);
         assert(read_count == send(sd, buf, read_count, 0));
@@ -198,10 +265,12 @@ int main(int argc, char **argv)
     return 0;
 
 exit:
-    if (sd > 0)
-        close(sd);
-    if (file)
-        fclose(file);
+    // this segfaulted lol (i think it's bc we might jump here before even defining file or sd).
+    // idk man, my error handling looks cool but feels suck :(
+    // if (file)
+    //     fclose(file);
+    // if (sd > 0)
+    //     close(sd);
 
     fprintf(stderr, "exit: %s\n", err_msg[terr]);
     exit(1);
